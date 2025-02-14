7326939e0758c0b260277de97cf314b2a81c0cc2
duck_master <duckeagle1280@gmail.com>
2025-02-14 17:19:05 -0500

    make client an instance method of Parser in backend/parser.py

--- backend/parser.py
+++ backend/parser.py
@@ -12,18 +12,22 @@ from huggingface_hub import InferenceClient
 
 class Parse:
     """
+    attributes:
+    * message
+    * json
+    * client
+
     methods:
     * __init__(self, message, json)
     * fill_json(self)
     """
-    client = InferenceClient("")
-    
     def __init__(self, message, json):
         self.message = message
         self.json = json
+        self.client = InferenceClient("")
 
     def fill_json(self):
-        resp = client.text_generation(
+        resp = self.client.text_generation(
             f'convert to JSON: {self.message}. please use the following schema: {self.json}" ',
             max_new_tokens=1000,  # arbitrary number
             seed=42,
