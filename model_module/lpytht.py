fc3403f1389eaefdcf7a9074193307036edaca34
duck_master <duckeagle1280@gmail.com>
2025-02-17 23:10:33 -0500

    add encoding and docstring to model_module/lpytht.py

--- model_module/lpytht.py
+++ model_module/lpytht.py
@@ -1,7 +1,20 @@
+"""
+A script to test the llama file
+
+Dependencies:
+* llama_cpp
+* yaml
+
+Global variables:
+* model_id
+* llm
+"""
+
+
 from llama_cpp import Llama
 import yaml
 
-with open("../config_module/config.yaml", "r") as file:
+with open("../config_module/config.yaml", "r", encoding = "utf-8") as file:
     configuration = yaml.safe_load(file)
 
 model_id = configuration["model_path"]
